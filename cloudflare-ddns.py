7cd0fe9cc804d8c5e8bcde4dfba59147fe463b90
Irfan Hakim <irfanhakim.as@yahoo.com>
2026-03-04 21:04:55 +0800

    Bump version to 1.0.3

--- cloudflare-ddns.py
+++ cloudflare-ddns.py
@@ -6,7 +6,7 @@
 #                A small, 🕵️ privacy centric, and ⚡
 #                lightning fast multi-architecture Docker image for self hosting projects.
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from string import Template
 from urllib.parse import urlparse
